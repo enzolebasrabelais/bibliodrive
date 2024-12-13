f6943cba07ffd799693c98280d52bfafd194cea2
enzolebasrabelais <enzolebas190106@gmail.com>
2024-12-13 15:08:55 +0100

    Fin lister_livres

--- lister_livres.php
+++ lister_livres.php
@@ -1,12 +1,13 @@
 <?php
 require_once('connexion_bibliodrive.php');
-$stmt = $connexion->prepare("SELECT titre, anneeparution FROM livre INNER JOIN auteur ON (livre.noauteur = auteur.noauteur) where auteur.nom=:nom");
+$stmt = $connexion->prepare("SELECT nolivre, titre, anneeparution FROM livre INNER JOIN auteur ON (livre.noauteur = auteur.noauteur) where auteur.nom=:nom ORDER BY anneeparution");
 $nom = $_POST["rchAuteur"];
-$prenom = "Jacques";
 $stmt->bindValue(":nom", $nom); // pas de troisième paramètre STR par défaut
 $stmt->setFetchMode(PDO::FETCH_OBJ);
 // Les résultats retournés par la requête seront traités en 'mode' objet
 $stmt->execute();
-$enregistrement = $stmt->fetch(); // boucle while inutile
-echo '<h1>', $enregistrement->titre, ' ', ' ', $enregistrement->anneeparution,'</h1>';
+while($enregistrement = $stmt->fetch())
+{
+echo '<h1>',"<a href='localhost/bibliodrive/detail.php?nolivre=".$enregistrement->nolivre."'>".$enregistrement->titre, ' ', ' ', '(', $enregistrement->anneeparution, ')', "</a>",'</h1>';
+}
 ?>
