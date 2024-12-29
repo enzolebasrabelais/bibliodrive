cbd5ecf6ee4af631b7be8e78041e2c8259b09f91
enzolebasrabelais <enzolebas190106@gmail.com>
2024-12-29 17:10:10 +0100

    Début accueil.php

---
+++ accueil.php
@@ -0,0 +1,23 @@
+<div class="row">
+<div class="col-sm-9">
+<?php
+include ('entete.html')
+?>
+</div>
+<div class="col-sm-3">
+
+</div>
+</div>
+
+<div class="row">
+<div class="col-sm-9">
+<?php
+include ('carrousel.php')
+?>
+</div>
+<div class="col-sm-3">
+<?php
+include ('se_connecter.php')
+?>
+</div>
+</div>
